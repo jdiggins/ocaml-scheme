d893fab1667ef37c8557f69350d228edff4ab57c
John Diggins <j.c.diggins@gmail.com>
2019-01-01 17:24:44 -0500

    create file to do basic fuzz

---
+++ fuzz.ml
@@ -0,0 +1,28 @@
+open Type
+open Env
+open Eval
+open Printf
+
+(* This is work in progress. Currently only sends strings to the eval*)
+
+(* This file is for fuzzing ocaml-scheme with afl fuzzer. To fuzz, do the following:
+
+make
+mkdir input
+echo This is a string  > input/testcase
+mkdir output
+afl-fuzz -i input -o output ./fuzz.native 
+
+*)
+
+let run_with input =
+  Eval.init_env();
+  let instr = input_line input in
+  let str = "(" ^ instr ^ ")" in
+  let e = Parser.expr Lexer.token (Lexing.from_string str) in
+  Type.show (eval e Env.global_env)
+  
+let start_fuzzing() =
+  run_with stdin;;
+
+start_fuzzing()
